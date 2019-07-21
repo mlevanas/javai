f7e62a56d43b5dc8c1ed9704f382c9baa9b404e1
Mindaugas <mndgslevanas@gmail.com>
2019-07-21 10:35:10 +0300

    Added foreign key constraints
    Set DB engine to InnoDB explicitly
    Added foreign key constraints for `buyers` and `cultures` tables

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -3,12 +3,12 @@ CREATE TABLE IF NOT EXISTS `#__javai_buyers`(
 	Name VARCHAR(255) NOT NULL,
 	Address VARCHAR(255),
 	Other_info TEXT
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `#__javai_cultures`(
 	Id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	Name VARCHAR(255) NOT NULL
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `#__javai_invoices`(
 	Id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -20,5 +20,8 @@ CREATE TABLE IF NOT EXISTS `#__javai_invoices`(
 	Humidity DECIMAL(18, 2),		/*drėgnumas*/
 	Proteins DECIMAL(18, 2),		/*baltymai*/
 	Gluted DECIMAL(18, 2),			/*glitimas*/
-	Price DECIMAL(18, 2) NOT NULL 		/*tos dienos kaina*/
-);
+	Price DECIMAL(18, 2) NOT NULL, 		/*tos dienos kaina*/
+	
+	FOREIGN KEY fk_suppliers(SupplierId) REFERENCES `#__javai_buyers`(Id) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY fk_cultures(CultureId) REFERENCES `#__javai_cultures`(Id) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=InnoDB;
